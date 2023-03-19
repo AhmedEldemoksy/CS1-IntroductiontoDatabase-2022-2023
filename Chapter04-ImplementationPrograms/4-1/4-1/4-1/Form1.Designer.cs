52493930d7f99badd1ff9a54d1f07e9cd72f48c6
ahmedeldemoksy <engahmedeldemoksy@gmail.com>
2023-03-19 09:32:28 +0200

    Chapter4FirstProgram

---
+++ Chapter04-ImplementationPrograms/4-1/4-1/4-1/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace _4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(517, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(517, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(685, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "رقم الطالب";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(685, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "اسم الطالب";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(361, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(303, 323);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 51);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "استعراض";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(673, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 51);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "حفظ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(544, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 51);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "عرض";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(397, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 51);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "عرض كلى";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(544, 360);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(151, 51);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "خروج";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 490);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Chapter04-ImplementationPrograms/4-1/4-1/4-1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace _4_1
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds = new DataSet();
+        SqlDataAdapter da;
+        SqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string strconnection = "data source=DESKTOP-LMOVISH; initial catalog=master; integrated security=true";
+            conn = new SqlConnection(strconnection);
+            conn.Open();
+
+            try
+            {
+                SqlCommand com1 = new SqlCommand("Create database db", conn);
+                com1.ExecuteNonQuery();
+
+                com1.Connection.ChangeDatabase("db");
+
+                SqlCommand com2 = new SqlCommand("Create table t1 (id int primary key, name varchar(50),images image)", conn);
+                com2.ExecuteNonQuery();
+
+            }
+            catch(Exception e1)
+            {
+                conn.ChangeDatabase("db");
+            }
+            da = new SqlDataAdapter("select * from t1", conn);
+            SqlCommandBuilder x = new SqlCommandBuilder(da);
+            da.Fill(ds,"t1");
+           
+            dataGridView1.DataSource = ds.Tables["t1"];
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image Files (*.bmp; *.png; *.jpg)| *.bmp; *.png; *.jpg";
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataRow r = ds.Tables["t1"].NewRow();
+            r["id"] = int.Parse(textBox1.Text);
+            r["name"] = textBox2.Text;
+            MemoryStream ms = new MemoryStream();
+            pictureBox1.Image.Save(ms,ImageFormat.Bmp);
+            byte[] arr = ms.ToArray();
+            r["images"] = arr;
+            ds.Tables["t1"].Rows.Add(r);
+            da.Update(ds, "t1");
+            clear();
+        }
+
+        void clear()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            pictureBox1.Image.Dispose();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (DataRow r in ds.Tables["t1"].Rows)
+            {
+                if (Convert.ToInt32(r["id"])==int.Parse(textBox1.Text))
+                {
+                    textBox2.Text = Convert.ToString(r["name"]);
+                    byte[] arr = (byte[])(r["images"]);
+                    MemoryStream ms = new MemoryStream(arr);
+                    Bitmap x = new Bitmap(ms);
+                    pictureBox1.Image = x;
+
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ds.Tables["t1"];
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
